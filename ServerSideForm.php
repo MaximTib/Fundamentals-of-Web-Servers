b4f97401f828f87ff27d159f55514e5250d12ab4
MaximTib <70660977+MaximTib@users.noreply.github.com>
2020-09-30 15:56:49 -0500

    attempt #4

--- ServerSideForm.php
+++ ServerSideForm.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
     <body>
-        Welcome <?php echo $_POST["fname"] $_POST["lname"]; ?>!<br>
-        It's good to know that you are <?php echo $_POST["age"]; ?>years old.<br><br>
+        Welcome <?php echo $_POST['fname'] $_POST['lname']; ?>!<br>
+        It's good to know that you are <?php echo $_POST['age']; ?>years old.<br><br>
 
-        You said: "<?php echo $_POST["form1"]; ?>"<br>
+        You said: "<?php echo $_POST['form1']; ?>"<br>
     </body>
 </html>
