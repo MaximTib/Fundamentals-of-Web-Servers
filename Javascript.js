ac3820f19df9e12dcd90c42a37669124d24018f5
MaximTib <70660977+MaximTib@users.noreply.github.com>
2020-09-30 13:33:19 -0500

    Corrected issue with JS code

--- Javascript.js
+++ Javascript.js
@@ -1,41 +1,34 @@
-function OnlyLetters(fname,lname)
-            {
-                var letters = /^[A-Za-z]+$/;
-                if((fname.value.match(letters)) && (lname.value.match(letters)))
-                {
-                    {
-                        return true;
-                    }
-                }
-                else
-                {
-                alert("Names can only contain letters");
-                return false;
-                }
-            } 
-
-function NumOnly(age)
-            {
-                var numbers = /^[0-9]+$/;
-                x = document.getElementById("age").value;
-
-                if((x.value.match(numbers))
-                {
-                    return true;
-                }
-
-                else
-                {
-                    alert("Enter a valid number");
-                    return false;
-                }
-            }
-function NewImage()
+function OnlyLetters(fname, lname) {
+    var letters = /^[A-Za-z]+$/;
+    if ((fname.value.match(letters)) && (lname.value.match(letters))) {
         {
-            document.getElementById("img1").src = "images/Corgi_image.jpg";
+            return true;
         }
+    }
+    else {
+        alert("Names can only contain letters");
+        return false;
+    }
+}
 
-        function OldImage()
-        {
-            document.getElementById("img1").src = "images/image_of_a_dog.jpg";
-        }  
+function NumOnly() {
+    var numbers = /^[0-9]+$/;
+    x = document.getElementById("age");
+
+    if ((x.value.match(numbers)) && (x >= 0) && (x <= 200)) {   
+        alert("valid");
+        return true;
+    }
+
+    else {
+        alert("Enter a valid number");
+        return false;
+    }
+}
+function NewImage() {
+    document.getElementById("img1").src = "images/Corgi_image.jpg";
+}
+
+function OldImage() {
+    document.getElementById("img1").src = "images/image_of_a_dog.jpg";
+}  
