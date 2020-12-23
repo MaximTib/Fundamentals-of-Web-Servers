6a225a8085f9ad8cf8d2937505f7786c57683a7f
MaximTib <70660977+MaximTib@users.noreply.github.com>
2020-12-22 19:50:15 -0500

    Update gpio.php

--- forms/gpio.php
+++ forms/gpio.php
@@ -4,7 +4,7 @@
         <body style="background-color:lightgrey;">
             <h1>Welcome to "Play with GPIO"</h1>
             <h2>Flip some switches and see what they do</h2>
-                <form action="gpio.php">
+                <form action="gpio.php" method="post">
                     <input type="radio" id="LEDON" name="LEDstate" value="ON">
                     <label for="LEDON">LED HIGH</label><br>
                     <input type="radio" id="LEDOFF" name="LEDstate" value="OFF">
@@ -13,46 +13,23 @@
                 </form>
 
                 <div>
-                <p onmouseover="BackColor()">
-                <?php 
-                function BackColor() {
-                    $output = shell_exec('gpio mode 1 in');
-                    echo "<div>$output</div>";
-                    $output = shell_exec('gpio mode 2 in');
-                    echo "<div>$output</div>";
-                    $output = shell_exec('gpio mode 3 in');
-                    echo "<div>$output</div>";
-                    $output = shell_exec('gpio mode 4 in');
-                    echo "<div>$output</div>";
-                
-                    //$pin4 = shell_exec('gpio read 4'); 
-                    //$pin3 = shell_exec('gpio read 3');
-                    //$pin2 = shell_exec('gpio read 2');
-                    $pin1 = shell_exec('gpio read 1');
-
-                    if ($pin1==0) { //&& $pin3==0 && $pin2==1 && $pin1==1) {
-                        return '"style="background-color:Orange;"';
-                    } //elseif ($pin4==0 && $pin3==1 && $pin2==1 && $pin1==1) {
-                    //    return '"style="background-color:Violet;"';                       
-                    //} elseif ($pin4==1 && $pin3==1 && $pin2==1 && $pin1==0             ) {
-                    //    return '"style="background-color:Yellow;"';                       
-                    //} elseif ($pin4==1 && $pin3==0 && $pin2==0 && $pin1==0) {
-                    //    return '"style="background-color:Blue;"';   
-                    else {
-                        return '"style="background-color:Blue;"';
-                    }
-                }?>
-                </p>
-                </div>
+                <p onmouseover="BackColor()">Choose a color to display as background of your submission form by selecting the correct binary code on the switches.
                 <br/>
-                Choose a color to display as background of your submission form by selecting the correct binary code on the switches.
                 <br/>Orange = 3
                 <br/>Violet = 7
                 <br/>Yellow = 14
                 <br/>Blue = 8
                 <br/>
+                </p>
+                </div>
 
                 <?php
+                if (empty($_POST['LEDstate'])) {
+                    $LEDerr = "";
+                } else {
+                    $LED = $_POST['LEDstate'];
+                }
+                
                 $output = shell_exec('gpio mode 8 out');
                 echo "<div>$output</div>";
                 if ($LED == "ON") {
